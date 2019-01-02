9df03e98d73f05136966ba48e288dae1bcb9a0f3
Arylo Yueng <arylo.open@gmail.com>
2019-01-02 10:25:43 +0800

    fix: fixed test fail

--- test/parse.spec.ts
+++ test/parse.spec.ts
@@ -7,14 +7,14 @@ test.beforeEach(async () => {
     filepath = await copyFileMacro("raw.file");
 });
 
-test("Read File", (t) => {
+test("Read File with JSON type", (t) => {
     const str = config.readFile(filepath).toObject();
     t.is(typeof str, "string");
     const obj = config.readFile(filepath, { type: "json" }).toObject();
     t.is(typeof obj, "object");
 });
 
-test("Read File", (t) => {
+test("Read File with RAW type", (t) => {
     const str1 = config.readFile(filepath).toObject();
     const str2 = config.readFile(filepath, { type: "raw" }).toObject();
     t.is(str1, str2);
