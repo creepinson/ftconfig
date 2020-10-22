e97a731f2337857043c788e5067e1323987c4ab9
wyhmichael <wyh.michael@gmail.com>
2020-10-22 01:23:14 -0700

    fix: lint

--- lib/options.d.ts
+++ lib/options.d.ts
@@ -1,4 +1,14 @@
-type BufferEncoding = "ascii" | "utf8" | "utf-8" | "utf16le" | "ucs2" | "ucs-2" | "base64" | "latin1" | "binary" | "hex";
+type BufferEncoding =
+    | "ascii"
+    | "utf8"
+    | "utf-8"
+    | "utf16le"
+    | "ucs2"
+    | "ucs-2"
+    | "base64"
+    | "latin1"
+    | "binary"
+    | "hex";
 
 export interface IReadFileOptions extends IReadOptions {
     encoding?: BufferEncoding;
